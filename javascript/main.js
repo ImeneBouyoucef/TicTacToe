c84d7930a8d14cd10afb3d2d682f4148dec9299d
imene <imene.bouyoucef2005@gmail.com>
2025-05-08 17:51:02 +0200

    text management in cells

--- javascript/main.js
+++ javascript/main.js
@@ -2,13 +2,28 @@ const playButton = document.getElementById("playButton");
 
 let turn = "X";
 let gameStarted = false;
+const cells = document.querySelectorAll(".cell");
 
 playButton.addEventListener("click", () => {
-    if (!gameStarted) {
+    if (!gameStarted) { 
         gameStarted = true;
         playButton.innerText = "Quit";
       } else {
         gameStarted = false;
         playButton.innerText = "Play";
+
+      cells.forEach(cell => {
+        cell.textContent = "";
+      });
+      turn = "X";
       }
   });
+
+cells.forEach(cell => {
+  cell.addEventListener("click", () => {
+    if (!gameStarted || cell.textContent !== "") return;
+
+    cell.textContent = turn;
+    turn = turn === "X" ? "O" : "X";
+    });
+});
