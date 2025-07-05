9ff040ed54c4156ea96f7f4d77026f4f832ea738
imene <imene.bouyoucef2005@gmail.com>
2025-07-05 11:08:22 +0200

    level 3

--- js/AIPlayer.js
+++ js/AIPlayer.js
@@ -17,6 +17,15 @@ export class AIPlayer {
                 this.playRandom(emptyCells); 
             }
         }
+        else if (this.level === 3) {
+            if (this.playToWin(cells)) {
+                return;
+            }
+            if (this.blockOpponent(cells)) {
+                return; 
+            }
+            this.playRandom(emptyCells); 
+        }
     }
 
     /**
@@ -29,11 +38,32 @@ export class AIPlayer {
     }
 
      /**
-     * Try to find a winning move. Return true if a move was played.
+     * Check if the AI ​​can win now and play that move if possible.
      * @param {NodeList} cells 
      * @returns {boolean}
      */
     playToWin(cells) {
+        return this.findAndPlayCombo(cells, this.symbol);
+    }
+
+     /**
+     * Check if the opponent can win and block this move
+     * @param {NodeList} cells 
+     * @returns {boolean}
+     */
+    blockOpponent(cells) {
+        const opponentSymbol = this.symbol === "X" ? "O" : "X";
+        return this.findAndPlayCombo(cells, opponentSymbol);
+    }
+
+    /**
+    * Searches for a potential winning combination for a given symbol
+    * and plays in the empty box if there are 2 symbols + 1 empty box.
+    * @param {NodeList} cells 
+    * @param {string} symbol 
+    * @returns {boolean}
+    */
+    findAndPlayCombo(cells, symbol) {
         const winCombos = [
             [0, 1, 2], [3, 4, 5], [6, 7, 8],
             [0, 3, 6], [1, 4, 7], [2, 5, 8],
@@ -43,15 +73,14 @@ export class AIPlayer {
         for (let combo of winCombos) {
             const [i, j, k] = combo;
             const values = [cells[i].textContent, cells[j].textContent, cells[k].textContent];
-            const symbols = [i, j, k];
+            const positions = [i, j, k];
 
-            // if there are 2 AI symbols and 1 empty one : play here
-            const countAI = values.filter(v => v === this.symbol).length;
+            const countSymbol = values.filter(v => v === symbol).length;
             const countEmpty = values.filter(v => v === "").length;
 
-            if (countAI === 2 && countEmpty === 1) {
-                const index = symbols[values.indexOf("")];
-                cells[index].textContent = this.symbol;
+            if (countSymbol === 2 && countEmpty === 1) {
+                const emptyIndex = positions[values.indexOf("")];
+                cells[emptyIndex].textContent = this.symbol;
                 return true;
             }
         }
